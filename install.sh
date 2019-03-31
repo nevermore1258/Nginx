6b29530bd9ab6440b999eb2f0602ebb17b213d40
TAO <seg9jk@TAOs-MacBook-Pro.local>
2019-04-01 01:14:32 +0700

    Nginx
    Autoinstall Nginx

---
+++ install.sh
@@ -0,0 +1,15 @@
+apt-get install software-properties-common -y
+add-apt-repository ppa:ondrej/php -y
+apt-get update -y
+apt-get -y install nginx php7.0-fpm php7.0-cli
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/nevermore1258/Nginx/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<h1 align="center">Server</h1>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/nevermore1258/Nginx/master/vps.conf"
+sed -i 's/listen = \/var\/run\/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.0/fpm/pool.d/www.conf
+service php7.0-fpm restart
+service nginx restart
